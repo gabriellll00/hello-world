5c61b846016ec6c23eae90ef3ef35311ec87c205
gabriellll00 <87761746+gabriellll00@users.noreply.github.com>
2021-07-29 17:09:11 -0300

    diferença entre o produto

---
+++ UriOnlineJudge/uri1007.py
@@ -0,0 +1,10 @@
+"""
+Diferença entre o produto de 4 valores.
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+dif_prod = (a * b) - (c * d)
+print(f'DIFERENCA = {dif_prod}')
