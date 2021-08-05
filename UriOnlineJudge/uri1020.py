bcf3c1c619a6841736c16689ffe431e44e471dc1
gabriellll00 <87761746+gabriellll00@users.noreply.github.com>
2021-08-05 16:05:41 -0300

    Create uri1020.py

---
+++ UriOnlineJudge/uri1020.py
@@ -0,0 +1,11 @@
+age_days = int(input())
+
+anos = age_days // 365
+resto_anos = age_days % 365
+meses = resto_anos // 30
+resto_meses = resto_anos % 30
+dias = resto_meses
+
+print(f'{anos} ano(s)')
+print(f'{meses} mes(es)')
+print(f'{dias} dia(s)')
