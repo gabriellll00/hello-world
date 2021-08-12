cc7d30e8adbc0916d13d962357e429c70cc9fbd5
gabriellll00 <87761746+gabriellll00@users.noreply.github.com>
2021-08-12 08:55:42 -0300

    contagem regressiva com laço for

--- curso-python/ex046.py
+++ curso-python/ex046.py
@@ -1,1 +1,8 @@
-
+from time import sleep
+print('\033[1;33m==CONTAGEM REGRESSIVA PARA A QUEIMA DE FOGOS==\033[m')
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('\033[33mPAPAPUPU🎆🎇PAPUL🎆🎇🎇🎆FIIILLLPUUUUUPOWPOWPOW\n'
+      'POW🎇🎇🎇🎆🎆🎆PAPAPA🎇🎆🎇🎆🎇🎆🎇🎆🎇TATATATATAFIIIIILLLFIIIIILLLLFIIIIIIILLL\n'
+      'POOOWWWWWW🎇🎆🎇🎆🎇🎆🎇🎇🎆🎇🎆🎇🎆🎇🎆🎇PAPAPAPAPUPUPUPUPU🎉🎉🎉🎊🎊🎊🎉🎉🎉\033[m')
