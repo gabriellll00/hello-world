6ba5347c87aa2c778624feb8ec964618b7f888d5
gabriellll00 <87761746+gabriellll00@users.noreply.github.com>
2021-08-26 08:55:15 -0300

    Update ex055.py

--- curso-python/ex055.py
+++ curso-python/ex055.py
@@ -1,6 +1,16 @@
 peso1 = 0
-for n in range(5):
+peso2 = 0
+
+for n in range(1, 6):
     peso = float(input('Digite um peso: '))
     if peso > peso1:
         peso1 = peso
+        print(peso1)
+    if peso2 == 0:
+        peso2 = peso
+        print(peso2)
+    elif peso < peso2:
+        peso2 = peso
+        print(peso2)
 print(f'O maior peso é {peso1}.')
+print(f'O menor peso é {peso2}.')
