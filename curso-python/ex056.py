663ea4f472646bbd35a351c4d340530a5da9482c
gabriellll00 <87761746+gabriellll00@users.noreply.github.com>
2021-08-26 09:11:14 -0300

    Update ex056.py

--- curso-python/ex056.py
+++ curso-python/ex056.py
@@ -1,31 +1,32 @@
 med_idad = 0
 mul_menor = 0
 age = 0
-mais_velho = None
+mais_velho = ''
 num_mul = 0
 
-for i in range(2):
+for i in range(4):
     nome = input('Digite o nome: ')
     idade = int(input('Digite uma idade: '))
+    print('Feminino digite "f"/Masculino digite "m"')
     sexo = input('Seu sexo: ').strip().lower()
     med_idad += idade
-    if sexo == 'masculino':
-        if idade > age:
-            mais_velho = nome
-    if sexo == 'feminino':
+    if sexo == 'm' and idade > age:
+        age = idade
+        mais_velho = nome
+    elif sexo == 'f':
         num_mul += 1
         if idade < 21:
             mul_menor += 1
-print()
-print(f'A média de idade do grupo é {med_idad / 2}\n')
-if mais_velho is None:
-    print('Não existem homens nesse grupo.\n')
+
+print(f'A média de idade do grupo é {med_idad / 4} anos')
+if mais_velho == '':
+    print('Não existem homens nesse grupo.')
 else:
-    print(f'{mais_velho.title()} é o homem mais velho do grupo.\n')
+    print(f'{mais_velho.title()} é o homem mais velho do grupo.')
 if num_mul == 0:
-    print('Não tem mulheres nesse grupo.\n')
+    print('Não tem mulheres nesse grupo.')
 else:
     if mul_menor == 0:
-        print('Nenhuma mulher nesse grupo tem menos de 21 anos.\n')
+        print('Nenhuma mulher nesse grupo tem menos de 21 anos.')
     else:
-        print(f'{mul_menor} mulher(es) do grupo tem menos de 21 anos.\n')
+        print(f'{mul_menor} mulher(es) do grupo tem menos de 21 anos.')
